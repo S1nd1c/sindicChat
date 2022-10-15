d8f03f6c97ad92f48f6d0cbc512f4f3b69b3ed2e
Nil Folquer <nfolquer@gmail.com>
2022-10-15 23:05:14 +0200

    Revert "Solved problem with default icons"
    This reverts commit aead1c9472063e4c4f4bdc1bfe7b9f5e28ed2bfb.

--- src/components/Chat/TextBox.js
+++ src/components/Chat/TextBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 function TextBox() {
   return (
     <div className="absolute bottom-0 w-full border-t-2 border-violet-300 h-16 flex items-center justify-center px-5 py-2">
-        <input className='p-3 w-full h-full rounded focus:border-violet-600' placeholder='Escribe un mensaje...'/>
+        <input className='p-3 w-full h-full rounded' placeholder='Escribe un mensaje...'/>
     </div>
   )
 }
--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import {useRef} from 'react'
 import ChatCategory from './ChatCategory'
 import ChannelCategory from './ChannelCategory';
 
 function ChatList() {
+    const publicChannels = useRef(null);
+    const privateChannels = useRef(null);
+    const privateMessages = useRef(null);
+
   return (
     <div className='w-full h-full'>
        <ChannelCategory categoryName="CANALES PÚBLICOS" />
